fb24aa01bd6c232ee532ded8ea1deaafaadf3355
17clementst@caldewschool.com <17clementst@caldewschool.com>
2023-07-15 17:33:06 +0100

    1st

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿namespace LetsPlayPoker
+{ 
+    public class Card
+    {
+        public string Rank {get;}
+        public string Suit {get;}
+
+        public Card(string rank, string suit)
+        {
+            Rank = rank;
+            Suit = suit;
+        }
+
+        public override string ToString() 
+        {
+            return $"{Rank}{Suit}";
+        }
+    }
+
+    public class Deck
+    {
+        protected readonly string[] Ranks = {"A", "2", "3", "4", "5", "6", "7", "8",
+                                             "9", "10", "J", "Q", "K"};
+        protected readonly string[] Suits = { "♠", "♦", "♣", "♥" };
+        protected List<Card> DeckOfCards;
+        
+        public Deck()
+        {
+            DeckOfCards = new List<Card>();
+            GenerateDeck();
+        }
+
+        protected void GenerateDeck()
+        {
+            foreach (string suit in Suits)
+            {
+                foreach(string rank in Ranks)
+                {
+                    DeckOfCards.Add(new Card(rank, suit));
+                }
+            }
+        }
+
+        public void DisplayDeck()
+        {
+            foreach(Card card in DeckOfCards)
+            {
+                Console.WriteLine(card);
+            }
+        }
+
+        public void ShuffleDeck()
+        {
+            Random RandomInt = new Random();
+
+            for (int i = 0; i < DeckOfCards.Count; i++)
+            {
+                int RandomIndex = RandomInt.Next(0, DeckOfCards.Count);
+                Card TempCard = DeckOfCards[i];
+                DeckOfCards[i] = DeckOfCards[RandomIndex];
+                DeckOfCards[RandomIndex] = TempCard;
+            } 
+        }
+    }   
+
+    internal class Program
+    {
+        static void DisplayDeck(Deck DeckOfCards)
+        {
+            DeckOfCards.DisplayDeck();
+        }
+
+        static void ShuffleDeck(Deck DeckOfCards)
+        {
+            DeckOfCards.ShuffleDeck();
+        }
+
+        static void LetsPlayPoker()
+        {
+            Deck DeckOfCards = new Deck();
+            DisplayDeck(DeckOfCards);
+            ShuffleDeck(DeckOfCards);
+        }
+
+        static void Main(string[] args)
+        {
+            LetsPlayPoker();
+            Console.ReadLine();
+        }
+    }
+}
